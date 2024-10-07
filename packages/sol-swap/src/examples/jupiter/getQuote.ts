428234f7e3159f4ad769b8f0eb095de45ca52604
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2024-10-07 18:25:57 +0200

    update example

--- packages/sol-swap/src/examples/jupiter/getQuote.ts
+++ packages/sol-swap/src/examples/jupiter/getQuote.ts
@@ -2,9 +2,9 @@ import getJupiterQuote from '@/lib/jupiter/getJupiterQuote'
 import jupiterSwap from '@/lib/jupiter/jupiterSwap'
 
 const getQuote = async () => {
-  const inputMint = 'inputMint'
-  const outputMint = 'outputMint'
-  const inputAmountLamport = 1000000000
+  const inputMint = 'So11111111111111111111111111111111111111112' // SOL
+  const outputMint = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v' // USDC
+  const inputAmountLamport = 0.01 * 10 ** 9 // 0.01 SOL
   const quote = await getJupiterQuote(inputMint, outputMint, inputAmountLamport)
   if (typeof quote === 'string') {
     throw new Error(quote)
